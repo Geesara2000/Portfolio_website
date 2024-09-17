e602ff2e72f6ef98ab01b8fa921e94935bdc3ba8
Geesara2000 <geesaraimal12@gmail.com>
2024-09-17 18:47:18 +0530

    create footer section

--- portfolio_react/src/App.jsx
+++ portfolio_react/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Project from './components/Project/Project'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <About/>
       <Project/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
---
+++ portfolio_react/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+import footer_logo from '../../assets/logo.svg'
+import user_icon from '../../assets/user_icon.svg'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <hr />
+      
+      <p>© 2024 Geesara De Silva.All rights reserved.</p>
+
+    </div>
+  )
+}
+
+export default Footer
